1f5b5b3a0f3e655482bf12e424bb013a9bd2c9a2
Saurav Sabu <saurav.sabu9@gmail.com>
2024-04-21 14:45:04 +0530

    Added setup.py file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "QAwithGemini",
+    version="0.0.1",
+    author="Saurav Sabu",
+    author_email="saurav.sabu9@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
